78b346ce4678e9f4092657b22fc10260b7c32d0b
Skylar Barrera <skylar.barrera@gmail.com>
2018-11-19 21:50:16 -0500

    Added Robot vs user detection

--- app/src/main/java/edu/wm/cs/cs301/skylarbarrera/gui/PlayAnimationActivity.java
+++ app/src/main/java/edu/wm/cs/cs301/skylarbarrera/gui/PlayAnimationActivity.java
@@ -147,6 +147,7 @@ public class PlayAnimationActivity extends AppCompatActivity {
             Toast.makeText(this, "Animation Paused", Toast.LENGTH_SHORT).show();
             Log.v("Anim - PlayPause", "Animation Paused");
         } else {
+            // pausing
             paused = true;
             Toast.makeText(this, "Animation UnPaused", Toast.LENGTH_SHORT).show();
             Log.v("Anim - PlayPause", "Animation UnPaused");
